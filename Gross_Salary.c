6b2143ef49e045ec6c0abb00ee30f57b999a6630
Jagadeshjk <119720496+Jagadeshjk@users.noreply.github.com>
2022-12-09 10:43:16 +0530

    #include<stdio.h> int main() {     int bs,da,hra;     float grs;     scanf("%d",&bs);     if(bs<=10000)     {         da=(bs*80)/100;         hra=(bs*20)/100;     }     else if(bs<=20000)     {         da=(bs*90)/100;         hra=(bs*25)/100;     }     else if(bs>20000)     {         da=(bs*95)/100;         hra=(bs*30)/100;     }     grs=da+hra+bs;     printf("%.2f",grs); }

---
+++ Gross_Salary.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int bs,da,hra;
+    float grs;
+    scanf("%d",&bs);
+    if(bs<=10000)
+    {
+        da=(bs*80)/100;
+        hra=(bs*20)/100;
+    }
+    else if(bs<=20000)
+    {
+        da=(bs*90)/100;
+        hra=(bs*25)/100;
+    }
+    else if(bs>20000)
+    {
+        da=(bs*95)/100;
+        hra=(bs*30)/100;
+    }
+    grs=da+hra+bs;
+    printf("%.2f",grs);
+}
